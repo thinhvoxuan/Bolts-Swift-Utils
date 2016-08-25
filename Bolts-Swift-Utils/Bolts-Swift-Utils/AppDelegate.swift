3446a1431ed3c26d8cd655fb4a469e2cf9f44304
thinhvoxuan <thinhvoxuan@gmail.com>
2016-08-25 16:18:09 +0700

    init project

---
+++ Bolts-Swift-Utils/Bolts-Swift-Utils/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Bolts-Swift-Utils
+//
+//  Created by thinhvoxuan on 8/25/16.
+//  Copyright © 2016 Thinhvoxuan. All rights reserved.
+//
+
+import UIKit
+import FBSDKCoreKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions);
+        
+        return true
+    }
+    
+    func applicationDidBecomeActive(application: UIApplication) {
+        FBSDKAppEvents.activateApp()
+    }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        let handled = FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+        return handled
+    }
+}
+
---
+++ Bolts-Swift-Utils/Bolts-Swift-Utils/Utils/FacebookUtils.swift
@@ -0,0 +1,13 @@
+//
+//  FacebookUtils.swift
+//  Bolts-Swift-Utils
+//
+//  Created by thinhvoxuan on 8/25/16.
+//  Copyright © 2016 Thinhvoxuan. All rights reserved.
+//
+
+import UIKit
+
+class FacebookUtils: NSObject {
+
+}
